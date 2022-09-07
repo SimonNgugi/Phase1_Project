5e66aa2d6fab4023c3641155b0dbfd41db6bc626
Simon Ngugi Ngigi <simonngugi88@gmail.com>
2022-09-07 17:06:38 +0300

    aboutUs

--- index.js
+++ index.js
@@ -16,3 +16,20 @@ search.addEventListener("click", () => {
         }
     })
 })
+
+let homeContainer = document.getElementById("container").innerHTML
+let newContent = document.getElementById("container")
+let aboutUsButton = document.getElementById("aboutUs")
+aboutUsButton.addEventListener("click", () => {
+    console.log(homeContainer)
+    newContent.innerHTML = "<div id='aboutUs' style='margin-top:100px;font-size: 40px;'><h1>What is Lorem Ipsum?</h1><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>"
+    form.style.visibility = "hidden"
+})
+
+let homeButton = document.getElementById("home")
+homeButton.addEventListener("click", () => {
+    newContent.innerHTML = homeContainer
+    form.style.visibility = "visible"
+})
+
+let form = document.getElementById("form")
